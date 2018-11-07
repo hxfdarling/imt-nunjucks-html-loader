291a035b5e72eacbb5f668fba353e5f7a4b6ac10
edenhliu <edenhliu@tencent.com>
2018-11-07 19:27:15 +0800

    feat: support relative path to search extend template

--- index.js
+++ index.js
@@ -1,71 +1,69 @@
-var utils = require('loader-utils');
-var fs = require('fs');
-var path = require('path');
-var nunjucks = require('nunjucks');
+const loaderUtils = require('loader-utils');
+const fs = require('fs');
+const path = require('path');
+const nunjucks = require('nunjucks');
 
-var NunjucksLoader = nunjucks.Loader.extend({
-    //Based off of the Nunjucks 'FileSystemLoader' 
+const NunjucksLoader = nunjucks.Loader.extend({
+  // Based off of the Nunjucks 'FileSystemLoader'
 
-    init: function(searchPaths, sourceFoundCallback) {
-    	this.sourceFoundCallback = sourceFoundCallback;
-        if(searchPaths) {
-            searchPaths = Array.isArray(searchPaths) ? searchPaths : [searchPaths];
-            // For windows, convert to forward slashes
-            this.searchPaths = searchPaths.map(path.normalize);
-        }
-        else {
-            this.searchPaths = ['.'];
-        }
-    },
-
-    getSource: function(name) {
-    	var fullpath = null;
-        var paths = this.searchPaths;
-
-        for(var i=0; i<paths.length; i++) {
-            var basePath = path.resolve(paths[i]);
-            var p = path.resolve(paths[i], name);
-
-            // Only allow the current directory and anything
-            // underneath it to be searched
-            if(p.indexOf(basePath) === 0 && fs.existsSync(p)) {
-                fullpath = p;
-                break;
-            }
-        }
-
-        if(!fullpath) {
-            return null;
-        }
-
-        this.sourceFoundCallback(fullpath);
-
-        return { 
-			src: fs.readFileSync(fullpath, 'utf-8'),
-			path: fullpath,
-			noCache: this.noCache 
-		};
+  init(searchPaths, sourceFoundCallback) {
+    this.sourceFoundCallback = sourceFoundCallback;
+    if (searchPaths) {
+      searchPaths = Array.isArray(searchPaths) ? searchPaths : [searchPaths];
+      // For windows, convert to forward slashes
+      this.searchPaths = searchPaths.map(path.normalize);
+    } else {
+      this.searchPaths = ['.'];
     }
+  },
+
+  getSource(name) {
+    let fullpath = null;
+    const paths = this.searchPaths;
+
+    for (let i = 0; i < paths.length; i++) {
+      const p = path.resolve(paths[i], name);
+      // Only allow the current directory and anything
+      // underneath it to be searched
+      if (fs.existsSync(p)) {
+        fullpath = p;
+        break;
+      }
+    }
+
+    if (!fullpath) {
+      return null;
+    }
+
+    this.sourceFoundCallback(fullpath);
+
+    return {
+      src: fs.readFileSync(fullpath, 'utf-8'),
+      path: fullpath,
+      noCache: this.noCache,
+    };
+  },
 });
 
 module.exports = function(content) {
-	this.cacheable();
-  
-	var callback = this.async();
-	var opt = utils.parseQuery(this.query);
+  this.cacheable();
 
-	var nunjucksSearchPaths = opt.searchPaths;
-	var nunjucksContext = opt.context;
+  const callback = this.async();
+  const opt = loaderUtils.getOptions(this);
+  const dir = path.dirname(this.resource);
 
-	var loader = new NunjucksLoader(nunjucksSearchPaths, function(path) {
-		this.addDependency(path);
-	}.bind(this));
+  const nunjucksSearchPaths = [dir, ...(opt.searchPaths || [])];
+  const nunjucksContext = opt.context;
 
-	var nunjEnv = new nunjucks.Environment(loader);
-	nunjucks.configure(null, { watch: false });
-	
-	var template = nunjucks.compile(content, nunjEnv);
-	html = template.render(nunjucksContext);
+  const loader = new NunjucksLoader(nunjucksSearchPaths, p => {
+    this.addDependency(p);
+  });
 
-	callback(null, html);
+  const nunjEnv = new nunjucks.Environment(loader);
+  nunjucks.configure(null, { watch: false });
+
+  const template = nunjucks.compile(content, nunjEnv);
+  const html = template.render(nunjucksContext);
+
+  callback(null, html);
 };
